5cd51180c5c52327c0794c0b96c8b67369df3bbb
Anthony <lahlah.anthony@gmail.com>
2022-03-06 17:50:32 +0100

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Anthony Lahlah on 06.03.22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
