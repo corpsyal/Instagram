fa24d8cc43642bcb3027543fbc586382a7dea04c
Anthony <lahlah.anthony@gmail.com>
2022-03-20 22:59:37 +0100

    Add firebase and kingfisher packages

--- Instagram/InstagramApp.swift
+++ Instagram/InstagramApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct InstagramApp: App {
+    
+    init(){
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
