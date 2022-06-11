fdfcd68b8e802c78effe255ed12346aed914e3d7
Anthony <lahlah.anthony@gmail.com>
2022-06-11 23:23:11 +0200

    add launch screen

--- Instagram/InstagramApp.swift
+++ Instagram/InstagramApp.swift
@@ -10,15 +10,32 @@ import Firebase
 
 @main
 struct InstagramApp: App {
+    @StateObject var authViewModel = AuthViewModel.shared
     
     init(){
         FirebaseApp.configure()
+        
+        let coloredAppearance = UINavigationBarAppearance()
+        coloredAppearance.backgroundColor = .systemBackground
+
+        // without scroll
+        UINavigationBar.appearance().standardAppearance = coloredAppearance
+        // when scroll
+        UINavigationBar.appearance().scrollEdgeAppearance = coloredAppearance
+        
+        let coloredTabBarAppearance = UITabBarAppearance()
+        coloredTabBarAppearance.backgroundColor = .systemBackground
+        UITabBar.appearance().standardAppearance = coloredTabBarAppearance
+        UITabBar.appearance().scrollEdgeAppearance = coloredTabBarAppearance
+        
+        // remove background from all text view
+        UITextView.appearance().backgroundColor = .clear
     }
     
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(AuthViewModel.shared)
+                .environmentObject(authViewModel)
         }
     }
 }
