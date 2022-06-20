64c02f05753db04602a1a4fd99ba31e70979b8bc
Anthony <lahlah.anthony@gmail.com>
2022-06-20 15:39:34 +0200

    change follow button structure

--- Instagram/View/Profile/ProfileActionButtonView.swift
+++ Instagram/View/Profile/ProfileActionButtonView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ProfileActionButtonView: View {
     @ObservedObject var viewModel: ProfileViewModel
+    private let cornerRadius: CGFloat = 3
+    
     var isFollowed: Bool {
         return viewModel.user.isFollowed ?? false
     }
@@ -22,27 +24,30 @@ struct ProfileActionButtonView: View {
                     .foregroundColor(Color.primary)
                     .font(.system(size: 14, weight: .semibold))
                     .overlay {
-                        RoundedRectangle(cornerRadius: 3)
+                        RoundedRectangle(cornerRadius: cornerRadius)
                             .stroke(lineWidth: 0.5)
                     }
             }
-    
+            
         } else {
             HStack {
                 Button {
                     isFollowed ? viewModel.unfollow() : viewModel.follow()
                 } label: {
-                    Text(isFollowed ? "Following" : "Follow")
+                    RoundedRectangle(cornerRadius: cornerRadius)
+                        .fill(isFollowed ? Color.clear : Color(UIColor.systemBlue))
                         .frame(maxWidth: .infinity)
-                        .padding(.vertical, 8)
-                        .foregroundColor(isFollowed ? Color.primary : Color.white)
-                        .font(.system(size: 14, weight: .semibold))
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 3)
-                                .stroke(isFollowed ? Color.primary : Color(UIColor.systemBlue),   lineWidth: 0.5)
-                        )
-                        .background(isFollowed ? Color.clear : Color(UIColor.systemBlue))
-                        .cornerRadius(3)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: cornerRadius)
+                                .stroke(isFollowed ? Color.primary : Color(UIColor.systemBlue), lineWidth: 0.5)
+                                .overlay {
+                                    Text(isFollowed ? "Following" : "Follow")
+                                        .padding(.vertical, 8)
+                                        .foregroundColor(isFollowed ? Color.primary : Color.white)
+                                        .font(.system(size: 14, weight: .semibold))
+                                }
+                            
+                        }
                 }
                 
                 Button {
@@ -54,7 +59,7 @@ struct ProfileActionButtonView: View {
                         .foregroundColor(Color.primary)
                         .font(.system(size: 14, weight: .semibold))
                         .overlay(
-                            RoundedRectangle(cornerRadius: 3)
+                            RoundedRectangle(cornerRadius: cornerRadius)
                                 .stroke(Color.primary, lineWidth: 0.5)
                         )
                 }
